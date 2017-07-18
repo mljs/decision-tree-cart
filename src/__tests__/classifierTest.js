06966c490a3bda30546b5a57dba2c1c6ae4c14cc
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-07-18 16:18:06 +0200

    test: update tests

--- src/__tests__/classifierTest.js
+++ src/__tests__/classifierTest.js
@@ -1,21 +1,21 @@
 import irisDataset from 'ml-dataset-iris';
 import {DecisionTreeClassifier as DTClassifier} from '..';
 
-describe('Decision Tree Classifier', function () {
-    var trainingSet = irisDataset.getNumbers();
-    var predictions = irisDataset.getClasses().map(elem => irisDataset.getDistinctClasses().indexOf(elem));
+var trainingSet = irisDataset.getNumbers();
+var predictions = irisDataset.getClasses().map(elem => irisDataset.getDistinctClasses().indexOf(elem));
 
-    var options = {
-        gainFunction: 'gini',
-        maxDepth: 10,
-        minNumSamples: 3
-    };
+var options = {
+    gainFunction: 'gini',
+    maxDepth: 10,
+    minNumSamples: 3
+};
 
-    var classifier = new DTClassifier(options);
-    classifier.train(trainingSet, predictions);
-    var result = classifier.predict(trainingSet);
+var classifier = new DTClassifier(options);
+classifier.train(trainingSet, predictions);
+var result = classifier.predict(trainingSet);
 
-    test('Decision Tree classifier with iris dataset', function () {
+describe('Decision Tree Classifier', () => {
+    test('Decision Tree classifier with iris dataset', () => {
         var correct = 0;
         for (var i = 0; i < result.length; ++i) {
             if (result[i] === predictions[i]) correct++;
@@ -25,14 +25,12 @@ describe('Decision Tree Classifier', function () {
         expect(score).toBeGreaterThanOrEqual(0.7);
     });
 
-    test('Export and import for decision tree classifier', function () {
+    test('Export and import for decision tree classifier', () => {
         var model = JSON.parse(JSON.stringify(classifier));
 
         var newClassifier = DTClassifier.load(model);
         var newResult = newClassifier.predict(trainingSet);
 
-        for (var i = 0; i < result.length; ++i) {
-            expect(newResult[i]).toBe(result[i]);
-        }
+        expect(newResult).toEqual(result);
     });
 });
--- src/__tests__/regressionTest.js
+++ src/__tests__/regressionTest.js
@@ -1,34 +1,32 @@
 import {DecisionTreeRegression as DTRegression} from '..';
 
-describe('Decision tree regression', function () {
-    var x = new Array(100);
-    var y = new Array(100);
-    var val = 0.0;
-    for (var i = 0; i < x.length; ++i) {
-        x[i] = val;
-        y[i] = Math.sin(x[i]);
-        val += 0.01;
-    }
+var x = new Array(100);
+var y = new Array(100);
+var val = 0.0;
+for (var i = 0; i < x.length; ++i) {
+    x[i] = val;
+    y[i] = Math.sin(x[i]);
+    val += 0.01;
+}
 
-    var reg = new DTRegression();
-    reg.train(x, y);
-    var estimations = reg.predict(x);
+var reg = new DTRegression();
+reg.train(x, y);
+var estimations = reg.predict(x);
 
-    test('Decision Tree classifier with sin function', function () {
-        for (i = 0; i < x.length; ++i) {
+describe('Decision tree regression', () => {
+    test('Decision Tree classifier with sin function', () => {
+        for (var i = 0; i < x.length; ++i) {
             expect(estimations[i]).toBeCloseTo(y[i], 0);
         }
     });
 
-    test('Export and import for decision tree classifier', function () {
+    test('Export and import for decision tree classifier', () => {
         var model = JSON.parse(JSON.stringify(reg));
 
         var newClassifier = DTRegression.load(model);
         var newEstimations = newClassifier.predict(x);
 
-        for (var i = 0; i < estimations.length; ++i) {
-            expect(newEstimations[i]).toBe(estimations[i]);
-        }
+        expect(newEstimations).toEqual(estimations);
     });
 });
 
