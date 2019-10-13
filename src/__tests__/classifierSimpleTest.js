a5eb6be0102d0b14a97ff3b8589f438b5e229fe2
Michaël Zasso <targos@protonmail.com>
2019-10-13 10:10:40 +0200

    Update classifierSimpleTest.js

--- src/__tests__/classifierSimpleTest.js
+++ src/__tests__/classifierSimpleTest.js
@@ -13,31 +13,31 @@ var trainingSet = [
   [0, 1, 0, 0],
   [0, 0, 1, 0],
   [0, 0, 0, 1]
-]
+];
 
-var predictions = [0, 1, 2, 3]
+var predictions = [0, 1, 2, 3];
 
 classifier.train(trainingSet, predictions);
 
 var result = classifier.predict(trainingSet);
 
 describe('Decision Tree Classifier', () => {
-  test('Decision Tree classifier with simple dataset', () => {
+  it('Decision Tree classifier with simple dataset', () => {
     var correct = 0;
     for (var i = 0; i < result.length; ++i) {
       if (result[i] === predictions[i]) correct++;
     }
 
     var score = correct / result.length;
-    expect(score).toBe(1.0)
+    expect(score).toBe(1.0);
   });
 
-  test('Export and import for decision simple classifier', () => {
+  it('Export and import for decision simple classifier', () => {
     var model = JSON.parse(JSON.stringify(classifier));
 
     var newClassifier = DTClassifier.load(model);
     var newResult = newClassifier.predict(trainingSet);
 
-    expect(newResult).toEqual(result);
+    expect(newResult).toStrictEqual(result);
   });
 });
